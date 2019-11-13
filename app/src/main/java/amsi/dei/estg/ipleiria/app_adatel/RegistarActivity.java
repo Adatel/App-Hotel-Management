65a2d850497a4059e39061b7858959053d0beb28
Alex <2180643@my.ipleiria.pt>
2019-11-13 17:43:17 +0000

    Login e Registar

---
+++ app/src/main/java/amsi/dei/estg/ipleiria/app_adatel/RegistarActivity.java
@@ -0,0 +1,14 @@
+package amsi.dei.estg.ipleiria.app_adatel;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegistarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registar);
+    }
+}
