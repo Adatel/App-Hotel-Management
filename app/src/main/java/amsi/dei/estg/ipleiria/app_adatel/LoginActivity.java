62a221d183568fc218f90867c2e85d959620ca05
Alex <2180643@my.ipleiria.pt>
2019-11-20 17:31:30 +0000

    Login Update

--- app/src/main/java/amsi/dei/estg/ipleiria/app_adatel/LoginActivity.java
+++ app/src/main/java/amsi/dei/estg/ipleiria/app_adatel/LoginActivity.java
@@ -1,16 +1,29 @@
-package amsi.dei.estg.ipleiria.app_adatel;
+package amsi.dei.estg.ipleiria.app_adatel.Login_e_Registar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import amsi.dei.estg.ipleiria.app_adatel.R;
+
 public class LoginActivity extends AppCompatActivity {
 
+    TextView registar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //retira a status bar
         getSupportActionBar().hide();
+
+        registar = findViewById(R.id.tvLinktoRegistar);
         setContentView(R.layout.activity_login);
+    }
+
+    public void onClick(View v) {
+        Intent intent = new Intent(this, RegistarActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/amsi/dei/estg/ipleiria/app_adatel/RegistarActivity.java
+++ app/src/main/java/amsi/dei/estg/ipleiria/app_adatel/RegistarActivity.java
@@ -1,14 +1,29 @@
-package amsi.dei.estg.ipleiria.app_adatel;
+package amsi.dei.estg.ipleiria.app_adatel.Login_e_Registar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import amsi.dei.estg.ipleiria.app_adatel.R;
 
 public class RegistarActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //retira a status bar
+        getSupportActionBar().hide();
+
         setContentView(R.layout.activity_registar);
+    }
+
+    public void onClickVoltar(View v) {
+        Intent intent = new Intent(this, LoginActivity.class);
+        // Arranca para a MainActivity
+        startActivity(intent);
+        finish();
     }
 }
