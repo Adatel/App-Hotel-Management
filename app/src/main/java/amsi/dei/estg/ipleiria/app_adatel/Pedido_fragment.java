ec688263c3f05dc83c43db0f296d0d8f9c97536f
Alex <2180643@my.ipleiria.pt>
2019-11-22 15:58:31 +0000

    Android base

---
+++ app/src/main/java/amsi/dei/estg/ipleiria/app_adatel/Pedido_fragment.java
@@ -0,0 +1,30 @@
+package amsi.dei.estg.ipleiria.app_adatel;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Pedido_fragment extends Fragment {
+
+
+    public Pedido_fragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_pedido_fragment, container, false);
+    }
+
+}
