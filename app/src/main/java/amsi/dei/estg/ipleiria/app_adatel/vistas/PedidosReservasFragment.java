a8d17ffa7a9ff42f8b0976425f4af86c94cc02b7
Afonso-Araujo <2180684@my.ipleiria.pt>
2019-12-09 16:06:19 +0000

    Singleton com fake data, lista das reservas a ser desenvolvido.

--- app/src/main/java/amsi/dei/estg/ipleiria/app_adatel/vistas/PedidosReservasFragment.java
+++ app/src/main/java/amsi/dei/estg/ipleiria/app_adatel/vistas/PedidosReservasFragment.java
@@ -1,14 +1,23 @@
 package amsi.dei.estg.ipleiria.app_adatel.vistas;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
+import java.util.ArrayList;
+
 import amsi.dei.estg.ipleiria.app_adatel.R;
+import amsi.dei.estg.ipleiria.app_adatel.adaptador.ListaReservaAdaptador;
+import amsi.dei.estg.ipleiria.app_adatel.models.Reserva;
+import amsi.dei.estg.ipleiria.app_adatel.models.SingletonGestaoHotel;
 
 
 /**
@@ -21,14 +30,32 @@ public class PedidosReservasFragment extends Fragment {
         // Required empty public constructor
     }
 
+    private ArrayList<Reserva> listaReservas;
+    private ListView lvlistaReservas;
+    private Reserva idReserva;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_listas, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_listas, container, false);
 
+        listaReservas = SingletonGestaoHotel.getInstance().getReservas();
+        lvlistaReservas = rootView.findViewById(R.id.lvLista);
+        lvlistaReservas.setAdapter(new ListaReservaAdaptador(getContext(), listaReservas));
+        lvlistaReservas.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Reserva tempLivro = (Reserva) parent.getItemAtPosition(position);
+                Toast.makeText(getContext(), "AQUI: " + tempLivro.getId(), Toast.LENGTH_SHORT).show();
+                idReserva = SingletonGestaoHotel.getInstance().getReserva(tempLivro.getId());
 
+                Intent intent = new Intent(getContext(), DetalhesReservaClienteActivity.class);
+                intent.putExtra(DetalhesReservaClienteActivity.CHAVE_ID, idReserva.getId());
+                startActivity(intent);
+            }
+        });
+        return rootView;
     }
-
 }
