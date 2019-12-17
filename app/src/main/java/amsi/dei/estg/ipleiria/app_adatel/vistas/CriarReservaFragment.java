34b7f9f6abcc8157e9f6fb20a1c0c1ebfa98a18f
Afonso-Araujo <2180684@my.ipleiria.pt>
2019-12-17 18:47:15 +0000

    DatePicker em comentario no CriarReservaFragment devido a erro

--- app/src/main/java/amsi/dei/estg/ipleiria/app_adatel/vistas/CriarReservaFragment.java
+++ app/src/main/java/amsi/dei/estg/ipleiria/app_adatel/vistas/CriarReservaFragment.java
@@ -1,6 +1,5 @@
 package amsi.dei.estg.ipleiria.app_adatel.vistas;
 
-
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -9,8 +8,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-
 import amsi.dei.estg.ipleiria.app_adatel.R;
 
 /**
@@ -18,18 +15,41 @@ import amsi.dei.estg.ipleiria.app_adatel.R;
  */
 public class CriarReservaFragment extends Fragment {
 
-    public FloatingActionButton fab;
 
     public CriarReservaFragment() {
         // Required empty public constructor
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_criar_reserva, container, false);
-    }
 
+    }
 }
+/*
+entrada = rootView.findViewById(R.id.etDataEntrada);
+
+        Calendar calendar = Calendar.getInstance();
+final int year = calendar.get(Calendar.YEAR);
+final int month = calendar.get(Calendar.MONTH);
+final int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        entrada.setOnClickListener(new View.OnClickListener() {
+
+@Override
+public void onClick(View v) {
+        DatePickerDialog datePickerDialog = new DatePickerDialog(
+        CriarReservaFragment.this, new DatePickerDialog.OnDateSetListener() {
+@Override
+public void onDateSet(DatePicker view, int year, int month, int day) {
+        month = month + 1;
+        String date = day + "/" + month + "/" + year;
+        entrada.setText(date);
+        }
+        }, year, month, day);
+        datePickerDialog.show();
+        }
+        });
+ */
