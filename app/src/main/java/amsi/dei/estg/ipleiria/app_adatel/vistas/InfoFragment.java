e63a354880dfdf965172f0bcc1ce3f1415f3f661
Afonso-Araujo <2180684@my.ipleiria.pt>
2020-01-09 15:48:57 +0000

    Fragment quem somos acabado.

--- app/src/main/java/amsi/dei/estg/ipleiria/app_adatel/vistas/InfoFragment.java
+++ app/src/main/java/amsi/dei/estg/ipleiria/app_adatel/vistas/InfoFragment.java
@@ -1,4 +1,4 @@
-package amsi.dei.estg.ipleiria.app_adatel;
+package amsi.dei.estg.ipleiria.app_adatel.vistas;
 
 
 import android.os.Bundle;
@@ -8,6 +8,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import amsi.dei.estg.ipleiria.app_adatel.R;
 
 
 /**
