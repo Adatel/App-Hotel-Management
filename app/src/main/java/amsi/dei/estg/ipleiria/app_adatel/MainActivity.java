6877ad88181ea9e76760a4999b45699a1d9b48af
Afonso-Araujo <2180684@my.ipleiria.pt>
2020-01-09 15:02:40 +0000

    Fragment quem somos acabado.

--- app/src/main/java/amsi/dei/estg/ipleiria/app_adatel/MainActivity.java
+++ app/src/main/java/amsi/dei/estg/ipleiria/app_adatel/MainActivity.java
@@ -128,6 +128,11 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 setTitle(menuItem.getTitle());
                 break;
 
+            case R.id.nav_quemSomos:
+                fragment = new InfoFragment();
+                setTitle(menuItem.getTitle());
+                break;
+
             case R.id.nav_ondeEstamos:
                 String uri = "http://maps.google.com/maps?q=loc:" + 39.734823  + "," + -8.821746;
                 Intent mapIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
