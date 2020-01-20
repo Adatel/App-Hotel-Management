786e8c5cb704c6774d37da331520a258fc93e353
Diana <diana.fp.gomes@gmail.pt>
2020-01-20 06:03:55 +0000

    Pedidos

--- app/src/main/java/amsi/dei/estg/ipleiria/app_adatel/vistas/DetalhesServicoQuartosActivity.java
+++ app/src/main/java/amsi/dei/estg/ipleiria/app_adatel/vistas/DetalhesServicoQuartosActivity.java
@@ -1,8 +1,10 @@
 package amsi.dei.estg.ipleiria.app_adatel.vistas;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -14,16 +16,42 @@ import android.widget.Spinner;
 import android.widget.Toast;
 import android.widget.Toolbar;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 import amsi.dei.estg.ipleiria.app_adatel.R;
+import amsi.dei.estg.ipleiria.app_adatel.models.Pedido;
+import amsi.dei.estg.ipleiria.app_adatel.models.SingletonGestaoHotel;
 
 public class DetalhesServicoQuartosActivity extends AppCompatActivity {
 
-    private int idServico;
+
+    public static final String CHAVE_IDPEDIDO = "idPedido";
+
+    private int idPedido;
+    private Pedido pedidoSelecionado;
+    private FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detalhes_servico_quartos);
+
+        fab = findViewById(R.id.fab);
+
+        // Recebe o id do pedido como parâmentro e vai buscar o pedido ao SingletonGestaoHotel pelo id
+        idPedido = getIntent().getIntExtra(CHAVE_IDPEDIDO,-1);
+
+
+        //System.out.println("--> " + dataSaida);
+
+        if(idPedido == -1){
+            setTitle("Fazer Pedido");
+            System.out.println("--> adicionar");
+            fab.setImageResource(R.drawable.ic_adicionar);
+        } else {
+            mostrarPedido(idPedido);
+            fab.setImageResource(R.drawable.ic_alterar);
+        }
 
         Spinner spinner;
         spinner = findViewById(R.id.spCategoria);
@@ -70,7 +98,63 @@ public class DetalhesServicoQuartosActivity extends AppCompatActivity {
             public void onNothingSelected(AdapterView <?> parent) {
             }
         });
+    }
 
+    private void mostrarPedido(int idPedido) {
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        if(idPedido != -1) {
+            MenuInflater inflater = getMenuInflater();
+            inflater.inflate(R.menu.menu_detalhes_reserva, menu);
+            return super.onCreateOptionsMenu(menu);
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        int id = item.getItemId();
+
+        if(id == R.id.itemRemover){
+            //Toast.makeText(this, "Remover", Toast.LENGTH_SHORT).show();
+            dialogRemover();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void dialogRemover() {
+
+        AlertDialog.Builder builder;
+
+        builder = new AlertDialog.Builder(this);
+        // Contruindo Alert Dialog
+        // Título
+        builder.setTitle("Cancelar Pedido")
+                // Messagem
+                .setMessage("Pretende mesmo cancelar o pedido?")
+                // 2 Botões
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        SingletonGestaoHotel.getInstance(getApplicationContext()).removerPedidoAPI(pedidoSelecionado);
+                        finish();
+                    }
+                })
+                .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // Não faz nada
+                    }
+                })
+                // Icon
+                .setIcon(android.R.drawable.ic_delete)
+                .show();
     }
 
 }
